4e0b9f56f6d84b6c89e7a9e380818f2da6233403
Steph-The-Dev <69237947+Steph-The-Dev@users.noreply.github.com>
2025-06-03 22:27:55 +0200

    played with index.vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,4 +11,5 @@ export default defineNuxtConfig({
     '@nuxt/scripts',
     '@nuxt/ui',
   ],
+  css: ['~/assets/css/main.css'],
 })
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="m-8 text-2xl">TEST für mich</h1>
+  </div>
+</template>
