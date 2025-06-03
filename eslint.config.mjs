9bb90f8002cd8f27afb4bed75b6368dcfe1378f4
Steph-The-Dev <69237947+Steph-The-Dev@users.noreply.github.com>
2025-06-03 11:21:42 +0200

    added prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,18 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
+import stylistic from '@stylistic/eslint-plugin';
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt([
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/quotes': ['error', 'single'],
+      'vue/multi-word-component-names': 'off',
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    },
+  },
+]);
